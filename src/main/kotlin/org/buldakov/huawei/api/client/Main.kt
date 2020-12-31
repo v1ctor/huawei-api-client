74d409452e06dbdb97be71e316448295215d6265
Victor Buldakov <victor@buldakov.org>
2020-12-31 15:10:56 +0000

    use password from command line

--- src/main/kotlin/org/buldakov/huawei/api/client/Main.kt
+++ src/main/kotlin/org/buldakov/huawei/api/client/Main.kt
@@ -1,11 +1,15 @@
 package org.buldakov.huawei.api.client
 
-fun main() {
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        println("please enter password")
+    }
+    val password = args[0]
     val modemApi = ModemApi("http://192.168.8.1")
 
     //val token = userApi.getSessionToken()
 
-    val sessionInfo = modemApi.login("admin", "")
+    val sessionInfo = modemApi.login("admin", password)
     while (true) {
         modemApi.getSms(sessionInfo)
         Thread.sleep(1000)

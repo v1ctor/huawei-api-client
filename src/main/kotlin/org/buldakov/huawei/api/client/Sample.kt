3f3a94b6616076b296f21e1298855e0fd4b616d0
Victor Buldakov <victor@buldakov.org>
2021-01-20 02:16:13 +0000

    add parameter to send sms

--- src/main/kotlin/org/buldakov/huawei/api/client/Sample.kt
+++ src/main/kotlin/org/buldakov/huawei/api/client/Sample.kt
@@ -5,15 +5,22 @@ import org.slf4j.LoggerFactory
 private val log = LoggerFactory.getLogger("huawei-api-client")
 
 fun main(args: Array<String>) {
+
     if (args.isEmpty()) {
         println("please enter password")
     }
     val password = args[0]
+    var phone: String? = null
+    if (args.size > 1) {
+        phone = args[1]
+    }
     val modemClient = ModemClient("http://192.168.8.1")
 
     modemClient.login("admin", password)
     val smsApi = SmsApi(modemClient)
-    smsApi.sendSms("+123456789", "Send from API")
+    if (phone != null) {
+        smsApi.sendSms(phone, "Send from API")
+    }
     while (true) {
         log.info(smsApi.getSms().toString())
         Thread.sleep(1000)
